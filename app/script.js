7374a299e0060b6d663f3c8f9e745216d9fabe78
mavolty <sadamyggakpernahp@gmail.com>
2021-08-12 23:31:27 +0700

    add library ui

--- app/script.js
+++ app/script.js
@@ -1,5 +1,5 @@
 // Color Thief
-import ColorThief from './dist/color-thief.mjs'
+import ColorThief from './dist/color-thief.mjs';
 
 const colorThief = new ColorThief();
 const displayImg = document.querySelector('img');
@@ -10,30 +10,33 @@ const playBtn = document.getElementById('js-play');
 const pauseBtn = document.getElementById('js-pause');
 const prevBtn = document.getElementById('js-prev');
 const nextBtn = document.getElementById('js-next');
-const displayBackground = document.querySelector('.background');
+const libraryBtn = document.getElementById('js-btn-lib');
+const displayBackground = document.querySelector('body');
 const displayTitle = document.querySelector('.player__song');
 const displayArtist = document.querySelector('.player__artist');
 const displayProgress = document.getElementById('js-progress');
 const displayProgressEl = document.getElementById('js-progress-el');
 const displayCurrentTime = document.querySelector('.progress__current-time');
 const displayDuration = document.querySelector('.progress__end-time');
-
+const displayLibrary = document.querySelector('.library');
+const app = document.querySelector('.app');
 
 displayImg.addEventListener('load', function () {
-    const primaryCol = colorThief.getColor(displayImg);
-    const secondaryCol = colorThief.getPalette(displayImg)[1];
-    const [firstCol, secondCol, thirdCol] = primaryCol;
-    const [firstSecCol, secondSecCol, thirdSecCol] = secondaryCol;
-    displayBackground.style.background = `linear-gradient(to right top, rgb(${firstCol}, ${secondCol}, ${thirdCol}) 0%, rgb(${firstSecCol}, ${secondSecCol}, ${thirdSecCol}) 100%)`
+  const primaryCol = colorThief.getColor(displayImg);
+  const secondaryCol = colorThief.getPalette(displayImg)[1];
+  const [firstCol, secondCol, thirdCol] = primaryCol;
+  const [firstSecCol, secondSecCol, thirdSecCol] = secondaryCol;
+  displayBackground.style.background = `linear-gradient(to right top, rgb(${firstCol}, ${secondCol}, ${thirdCol}) 0%, rgb(${firstSecCol}, ${secondSecCol}, ${thirdSecCol}) 100%)`;
+  libraryBtn.style.color = `rgb(${firstCol}, ${secondCol}, ${thirdCol})`;
 });
 
 class Songs {
-    constructor(name, title, artist) {
-        this.name = name;
-        this.title = title;
-        this.artist = artist;
-    }
-};
+  constructor(name, title, artist) {
+    this.name = name;
+    this.title = title;
+    this.artist = artist;
+  }
+}
 
 let isPlaying = false;
 
@@ -48,98 +51,111 @@ let songs = [drunk, tooSoon, blue, itKillsMe, bandaids];
 let startIndex = 0;
 
 function playSong() {
-    isPlaying = true;
-    pauseBtn.classList.add('show');
-    playBtn.classList.add('hide');
-    music.play();
-};
+  isPlaying = true;
+  pauseBtn.classList.add('show');
+  playBtn.classList.add('hide');
+  music.play();
+}
 
 function pauseSong() {
-    isPlaying = false;
-    pauseBtn.classList.remove('show');
-    playBtn.classList.remove('hide');
-    music.pause();
-};
+  isPlaying = false;
+  pauseBtn.classList.remove('show');
+  playBtn.classList.remove('hide');
+  music.pause();
+}
 
 function loadSong(song) {
-    displayImg.setAttribute('src', `img/${song.name}.jpg`);
-    music.setAttribute('src', `music/${song.name}.mp3`);
-    displayTitle.textContent = song.title;
-    displayArtist.textContent = song.artist;
-};
+  displayImg.setAttribute('src', `img/${song.name}.jpg`);
+  music.setAttribute('src', `music/${song.name}.mp3`);
+  displayTitle.textContent = song.title;
+  displayArtist.textContent = song.artist;
+}
 
 function prevSong() {
-    startIndex--;
-    if (startIndex < 0) {
-        startIndex = songs.length - 1;
-    };
-    loadSong(songs[startIndex]);
-    playSong();
-};
+  startIndex--;
+  if (startIndex < 0) {
+    startIndex = songs.length - 1;
+  }
+  loadSong(songs[startIndex]);
+  playSong();
+}
 
 function nextSong() {
-    startIndex++;
-    if (startIndex > songs.length - 1) {
-        startIndex = 0;
-    };
-    loadSong(songs[startIndex]);
-    playSong();
-};
+  startIndex++;
+  if (startIndex > songs.length - 1) {
+    startIndex = 0;
+  }
+  loadSong(songs[startIndex]);
+  playSong();
+}
 
 function progressBar(e) {
-    if (isPlaying) {
-        const {
-            currentTime,
-            duration
-        } = e.srcElement;
-        let progressPercent = currentTime / duration * 100;
-        displayProgress.style.width = `${progressPercent}%`;
+  if (isPlaying) {
+    const { currentTime, duration } = e.srcElement;
+    let progressPercent = (currentTime / duration) * 100;
+    displayProgress.style.width = `${progressPercent}%`;
 
-        let currentMinutes = Math.trunc(currentTime / 60);
-        let currentSeconds = Math.trunc(currentTime % 60);
-        if (currentMinutes < 10) {
-            currentMinutes = `0${currentMinutes}`;
-        }
-        if (currentSeconds < 10) {
-            currentSeconds = `0${currentSeconds}`;
-        }
-        displayCurrentTime.textContent = `${currentMinutes}:${currentSeconds}`
+    let currentMinutes = Math.trunc(currentTime / 60);
+    let currentSeconds = Math.trunc(currentTime % 60);
+    if (currentMinutes < 10) {
+      currentMinutes = `0${currentMinutes}`;
+    }
+    if (currentSeconds < 10) {
+      currentSeconds = `0${currentSeconds}`;
+    }
+    displayCurrentTime.textContent = `${currentMinutes}:${currentSeconds}`;
 
-        let durationMinutes = Math.trunc(duration / 60);
-        let durationSeconds = Math.trunc(duration % 60);
-        if (durationMinutes < 10) {
-            durationMinutes = `0${durationMinutes}`;
-        }
-        if (durationSeconds < 10) {
-            durationSeconds = `0${durationSeconds}`;
-        }
-
-        if (durationMinutes) {
-            displayDuration.textContent = `${durationMinutes}:${durationSeconds}`;
-        }
+    let durationMinutes = Math.trunc(duration / 60);
+    let durationSeconds = Math.trunc(duration % 60);
+    if (durationMinutes < 10) {
+      durationMinutes = `0${durationMinutes}`;
+    }
+    if (durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`;
     }
 
-    if (displayProgress.style.width == '100%') {
-        pauseBtn.classList.remove('show');
-        playBtn.classList.remove('hide');
-        pauseSong();
+    if (durationMinutes) {
+      displayDuration.textContent = `${durationMinutes}:${durationSeconds}`;
     }
+  }
+
+  if (displayProgress.style.width == '100%') {
+    pauseBtn.classList.remove('show');
+    playBtn.classList.remove('hide');
+    pauseSong();
+  }
 }
 
 function setProgressBar(e) {
-    playSong()
-    const width = displayProgressEl.clientWidth;
-    const clickX = e.offsetX;
-    const {
-        duration
-    } = music
-    const value = clickX / width * duration;
-    music.currentTime = value
+  playSong();
+  const width = displayProgressEl.clientWidth;
+  const clickX = e.offsetX;
+  const { duration } = music;
+  const value = (clickX / width) * duration;
+  music.currentTime = value;
 }
 
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
-pauseBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+pauseBtn.addEventListener('click', () =>
+  isPlaying ? pauseSong() : playSong()
+);
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 music.addEventListener('timeupdate', progressBar);
 displayProgressEl.addEventListener('click', setProgressBar);
+songs.map(el =>
+  displayLibrary.insertAdjacentHTML(
+    'beforeend',
+    `<div class="song">
+          <img class="song__img" src="img/${el.name}.jpg" alt="Drunk">
+          <div class="song__details">
+              <h2 class="song__title">${el.title}</h2>
+              <span class="song__artist">${el.artist}</span>
+          </div>
+      </div>`
+  )
+);
+libraryBtn.addEventListener('click', function () {
+  displayLibrary.classList.toggle('active');
+  app.classList.toggle('active');
+});
